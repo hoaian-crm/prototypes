87477240f6335c5eadd791c5ef6765fb12756606
hackermasterlord <126839836+hackermasterlord@users.noreply.github.com>
2023-11-29 14:01:48 +0700

    update formatter

---
+++ formatters/response.ts
@@ -0,0 +1,46 @@
+import { Messages } from "./messages";
+
+export type IMessage = {
+  code: number;
+  description?: string;
+  field?: string;
+  metaData?: string;
+}
+
+export type ResponseData<T extends any> = {
+  result: T | Array<T>;
+  total?: number;
+}
+
+export class Response<T extends any> {
+  messages: Array<IMessage>;
+  data: ResponseData<T>;
+
+  static findSuccess<T extends any>([result, count]: [result: Array<T>, number]): Response<T> {
+    return {
+      messages: [Messages.getSuccess],
+      data: {
+        result,
+        total: count,
+      }
+    }
+  }
+
+  static createSuccess<T extends any>(result: T) {
+    return {
+      messages: [Messages.createSuccess],
+      data: {
+        result,
+        total: 1
+      }
+    }
+  }
+
+  static updateSuccess<T extends any>(result: T) {
+    throw new Error("Mr.Van implement latter")
+  }
+
+  static deleteSuccess<T extends any>(result: T) {
+    throw new Error("Mr.Van implement latter")
+  }
+}
