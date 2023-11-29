f79f6762aa1ee86e28196d15cc02daefc1733fc7
hothanhvawn <vanho@playgroundvina.com>
2023-11-29 14:16:00 +0700

    update reponse

--- formatters/response.ts
+++ formatters/response.ts
@@ -1,29 +1,32 @@
-import { Messages } from "./messages";
+import { Messages } from './messages';
 
 export type IMessage = {
   code: number;
   description?: string;
   field?: string;
   metaData?: string;
-}
+};
 
 export type ResponseData<T extends any> = {
   result: T | Array<T>;
   total?: number;
-}
+};
 
 export class Response<T extends any> {
   messages: Array<IMessage>;
   data: ResponseData<T>;
 
-  static findSuccess<T extends any>([result, count]: [result: Array<T>, number]): Response<T> {
+  static findSuccess<T extends any>([result, count]: [
+    result: Array<T>,
+    number,
+  ]): Response<T> {
     return {
       messages: [Messages.getSuccess],
       data: {
         result,
         total: count,
-      }
-    }
+      },
+    };
   }
 
   static createSuccess<T extends any>(result: T) {
@@ -31,16 +34,28 @@ export class Response<T extends any> {
       messages: [Messages.createSuccess],
       data: {
         result,
-        total: 1
-      }
-    }
+        total: 1,
+      },
+    };
   }
 
   static updateSuccess<T extends any>(result: T) {
-    throw new Error("Mr.Van implement latter")
+    return {
+      messages: [Messages.updateSuccess],
+      data: {
+        result,
+        total: 1,
+      },
+    };
   }
 
   static deleteSuccess<T extends any>(result: T) {
-    throw new Error("Mr.Van implement latter")
+    return {
+      messages: [Messages.deleteSuccess],
+      data: {
+        result,
+        total: 1,
+      },
+    };
   }
 }
