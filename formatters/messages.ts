0a2c0ffb82b18b14e600256d5e3b7640232425d0
hackermasterlord <126839836+hackermasterlord@users.noreply.github.com>
2023-11-30 11:29:31 +0700

    Updaet prototype

---
+++ formatters/messages.ts
@@ -0,0 +1,22 @@
+export const Messages = {
+  getSuccess: {
+    code: 8,
+    description: 'get successfully',
+  },
+  createSuccess: {
+    code: 24,
+    description: 'create successfully',
+  },
+  updateSuccess: {
+    code: 14,
+    description: 'update successfully',
+  },
+  deleteSuccess: {
+    code: 20,
+    description: 'delete successfully',
+  },
+  mustHaveFile: {
+    code: 100,
+    description: 'must post file to upload',
+  },
+};
