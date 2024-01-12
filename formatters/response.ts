55389d6a654d60e1480faed0d047841e814feeb0
hackermasterlord <126839836+hackermasterlord@users.noreply.github.com>
2024-01-12 17:03:08 +0700

    Update

--- formatters/response.ts
+++ formatters/response.ts
@@ -3,7 +3,7 @@ import { BadRequestException } from '@nestjs/common';
 import { Messages } from './messages';
 
 export class IMessage {
-  code: number | number;
+  code: number | string;
   description?: string;
   field?: string;
   metaData?: string;
